83a119fb2d9f5926052515a055cdd9a623ff3171
david <davidmatovu88@gmail.com>
2023-05-20 12:27:19 +0300

    implement email sending with nodemailer

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,23 @@ const express = require('express');
 import mongoose from 'mongoose';
 // import express, { Application } from 'express';
 const reservationRouter = require('./routes/reservationRouter');
+const sendEmail = require('./nodemailer/emailTransporter'); // Import the sendEmail function
 const app = express();
 app.use(express.json());
 const port = 5000;
 
 app.use('/reservations', reservationRouter);
 
+// Route to trigger sending the email
+app.get('/send-email', (req, res) => {
+    sendEmail(); // Call the sendEmail function
+    res.send('Email sent successfully');
+});
+
 // Connect to the MongoDB database
 mongoose
     .connect(
-        'mongodb+srv://david:<password>@cluster0.bkgfjsx.mongodb.net/reservation-portal'
+        'mongodb+srv://david:david@cluster0.bkgfjsx.mongodb.net/reservation-portal'
     )
     .then(() => {
         console.log('Connected to database');
---
+++ src/nodemailer/emailTransporter.ts
@@ -0,0 +1,48 @@
+const path = require('path');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
+function sendEmail() {
+    // create reusable transporter object using the default SMTP transport
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'pmsmaven@gmail.com',
+            pass: 'pdzkxwrcmllyksqq',
+        },
+    });
+    // point to the template folder
+    const handlebarOptions = {
+        viewEngine: {
+            partialsDir: path.resolve(__dirname, '../views/'),
+            defaultLayout: false,
+        },
+        viewPath: path.resolve(__dirname, '../views/'),
+    };
+
+    // use a template file with nodemailer
+    transporter.use('compile', hbs(handlebarOptions));
+
+    const mailOptions = {
+        from: '"david" <david.rb.pmsmaven@gmail.com>', // sender address
+        to: 'davidmatovu88@gmail.com', // list of receivers
+        subject: 'New Reservation!',
+        template: 'email', // the name of the template file i.e email.handlebars
+        context: {
+            name: 'David', // replace {{name}} with David
+            company: 'My Company', // replace {{company}} with My Company
+        },
+    };
+
+    // trigger the sending of the E-mail
+    transporter.sendMail(mailOptions, function (error: any, info: any) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
+
+// Call the function to send the welcome email
+module.exports = sendEmail; // Export the sendEmail function
